847cafdb10ea9ab0f4ed87a4e22d1a9ef28515d6
Terry Brady <terrywbrady@gmail.com>
2021-08-06 10:51:02 -0700

    remove echo

--- billing/daily-billing.sh
+++ billing/daily-billing.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-echo 'script start'
 ${HOME}/bin/uc3-mysql.sh billing -- -e 'call update_billing_range();'
-echo 'after billing range'
 ${HOME}/bin/uc3-mysql.sh billing -- -e 'call update_object_size();'
-echo 'after object size'
 ${HOME}/bin/uc3-mysql.sh billing -- -e 'call update_audits_processed();'
-echo 'after audits processed'
 ${HOME}/bin/uc3-mysql.sh billing -- -e 'call update_ingests_processed();'
-echo 'after ingests processed'
