39f84a12a86f97c21365389ed7cbed22702b23f7
Terry Brady <terrywbrady@gmail.com>
2022-04-04 09:16:34 -0700

    fix path

--- billing/daily-billing.sh
+++ billing/daily-billing.sh
@@ -5,4 +5,4 @@ for i in 1 2 3 4 5; do ${HOME}/bin/uc3-mysql.sh billing -- -e 'call update_objec
 for i in 1 2 3 4 5; do ${HOME}/bin/uc3-mysql.sh billing -- -e 'call update_audits_processed();' && break || sleep 15; done
 for i in 1 2 3 4 5; do ${HOME}/bin/uc3-mysql.sh billing -- -e 'call update_ingests_processed();' && break || sleep 15; done
 
-for i in 1 2 3 4 5; do ${HOME}/bin/uc3-mysql.sh billing readwrite < daily_node_counts.sql && break || sleep 15; done
+for i in 1 2 3 4 5; do ${HOME}/bin/uc3-mysql.sh billing readwrite < billing/daily_node_counts.sql && break || sleep 15; done
