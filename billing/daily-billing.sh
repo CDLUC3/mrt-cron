067e8f2ce64f39bd9ee2afa4fe70b851ba4045f6
Terry Brady <terrywbrady@gmail.com>
2022-04-04 10:05:17 -0700

    identify failing query

--- billing/daily-billing.sh
+++ billing/daily-billing.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-for i in 1 2 3 4 5; do ${HOME}/bin/uc3-mysql.sh billing -- -e 'call update_billing_range();' && break || sleep 15; done
-for i in 1 2 3 4 5; do ${HOME}/bin/uc3-mysql.sh billing -- -e 'call update_object_size();' && break || sleep 15; done
-for i in 1 2 3 4 5; do ${HOME}/bin/uc3-mysql.sh billing -- -e 'call update_audits_processed();' && break || sleep 15; done
-for i in 1 2 3 4 5; do ${HOME}/bin/uc3-mysql.sh billing -- -e 'call update_ingests_processed();' && break || sleep 15; done
+for i in 1 2 3 4 5; do ${HOME}/bin/uc3-mysql.sh billing -- -e 'call update_billing_range();' && break || echo "Retry Q1-${i}" && sleep 15; done
+for i in 1 2 3 4 5; do ${HOME}/bin/uc3-mysql.sh billing -- -e 'call update_object_size();' && break || echo "Retry Q2-${i}" && sleep 15; done
+for i in 1 2 3 4 5; do ${HOME}/bin/uc3-mysql.sh billing -- -e 'call update_audits_processed();' && break || echo "Retry Q3-${i}" && sleep 15; done
+for i in 1 2 3 4 5; do ${HOME}/bin/uc3-mysql.sh billing -- -e 'call update_ingests_processed();' && break || echo "Retry Q4-${i}" && sleep 15; done
 
-for i in 1 2 3 4 5; do ${HOME}/bin/uc3-mysql.sh billing readwrite << EOF && break || sleep 15; done
+for i in 1 2 3 4 5; do ${HOME}/bin/uc3-mysql.sh billing readwrite << EOF && break || echo "Retry Q5-${i}" && sleep 15; done
 delete from 
   daily_node_counts 
 where 
