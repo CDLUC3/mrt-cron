b55c6902d198561940b5bbad102fa7b37c224624
Terry Brady <terrywbrady@gmail.com>
2022-04-04 09:13:08 -0700

    retry billing script

--- billing/daily-billing.sh
+++ billing/daily-billing.sh
@@ -1,44 +1,8 @@
 #!/bin/sh
 
-${HOME}/bin/uc3-mysql.sh billing -- -e 'call update_billing_range();'
-${HOME}/bin/uc3-mysql.sh billing -- -e 'call update_object_size();'
-${HOME}/bin/uc3-mysql.sh billing -- -e 'call update_audits_processed();'
-${HOME}/bin/uc3-mysql.sh billing -- -e 'call update_ingests_processed();'
+for i in 1 2 3 4 5; do ${HOME}/bin/uc3-mysql.sh billing -- -e 'call update_billing_range();' && break || sleep 15; done
+for i in 1 2 3 4 5; do ${HOME}/bin/uc3-mysql.sh billing -- -e 'call update_object_size();' && break || sleep 15; done
+for i in 1 2 3 4 5; do ${HOME}/bin/uc3-mysql.sh billing -- -e 'call update_audits_processed();' && break || sleep 15; done
+for i in 1 2 3 4 5; do ${HOME}/bin/uc3-mysql.sh billing -- -e 'call update_ingests_processed();' && break || sleep 15; done
 
-${HOME}/bin/uc3-mysql.sh billing readwrite << EOF
-
-delete from 
-  daily_node_counts 
-where 
-  as_of_date = date(now());
-
-insert into daily_node_counts (
-  as_of_date,
-  inv_node_id,
-  number,
-  object_count,
-  object_count_primary,
-  object_count_secondary,
-  file_count,
-  billable_size
-)
-select
-  date(now()),
-  n.id,
-  n.number,
-  count(inio.inv_object_id),
-  sum(case when role ='primary' then 1 else 0 end),
-  sum(case when role ='secondary' then 1 else 0 end),
-  sum(os.file_count),
-  sum(os.billable_size)
-from
-  inv.inv_nodes n
-inner join inv.inv_nodes_inv_objects inio 
-  on n.id = inio.inv_node_id
-inner join object_size os
-  on inio.inv_object_id = os.inv_object_id
-group by 
-  n.id,
-  n.number;
-
-EOF
+for i in 1 2 3 4 5; do ${HOME}/bin/uc3-mysql.sh billing readwrite < daily_node_counts.sql && break || sleep 15; done
