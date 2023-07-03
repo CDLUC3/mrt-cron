cfc00bc775bcc638d27fbd3ec53d933be6b4f2bf
Terry Brady <terrence.brady@ucop.edu>
2023-07-03 12:52:18 -0700

    viz

---
+++ viz/billing_viz.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+mkdir -p /tmp/viz
+uc3-mysql.sh billing -- -e "select * from owner_coll_mime_use_details;" | ruby make_json.rb > /tmp/viz/tmp.ndjson
---
+++ viz/make_json.rb
@@ -0,0 +1,18 @@
+require 'json'
+require 'time'
+
+headers = []
+ARGF.each_with_index do |line, i|
+  if i == 0
+    headers = line.strip!.split("\t") 
+ else
+    rec = {}
+    line.strip!.split("\t").each_with_index do |col, j|
+      d = col
+      d = Integer(col) if col =~ %r[^\d+$]
+      d = Time.parse(col).strftime("%Y-%m-%d %H:%M:%S %z") if col =~ %r[^\d\d\d\d\-\d\d\-\d\d$]
+      rec[headers[j]] = d
+    end
+    puts rec.to_json unless rec["date_added"] < "2022-07-01"
+  end
+end
