fae38f722305503b81ee5a209dfa84bd6a926969
Terry Brady <terrence.brady@ucop.edu>
2023-07-05 10:43:13 -0700

    update output file name

--- viz/billing_viz.sh
+++ viz/billing_viz.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 mkdir -p /tmp/viz
-uc3-mysql.sh billing -- -e "select * from owner_coll_mime_use_details;" | ruby make_json.rb > /tmp/viz/tmp.ndjson
+uc3-mysql.sh billing -- -e "select * from owner_coll_mime_use_details;" | ruby make_json.rb > billing.ndjson
