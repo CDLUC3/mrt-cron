0f38c9a7ab97ca7c8e1c3c8977a27ebf965257d3
Terry Brady <terrence.brady@ucop.edu>
2023-08-03 16:01:57 -0700

    comment update

--- viz/billing_viz.sh
+++ viz/billing_viz.sh
@@ -3,5 +3,5 @@
 # COLLHDATA=/dpr2/apps/mrt-cron/coll_health/data ./billing_viz.sh
 #
 # In dev...
-# COLLHDATA=$PWD billing_viz.sh
+# COLLHDATA=$PWD ./billing_viz.sh
 uc3-mysql.sh billing -- -e "select * from owner_coll_mime_use_details;" | ruby make_json.rb > ${COLLHDATA}/billing.ndjson
