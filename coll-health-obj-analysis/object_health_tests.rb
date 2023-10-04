3b08dac837ef502b53ece8e15a1913a5b63a4644
Terry Brady <terrence.brady@ucop.edu>
2023-10-04 15:48:31 -0700

    sample tests

--- coll-health-obj-analysis/object_health_tests.rb
+++ coll-health-obj-analysis/object_health_tests.rb
@@ -8,7 +8,11 @@ class ObjectHealthTests
   end
 
   def run_tests(obj)
-    obj[:tests] = {}
+    obj[:tests] = {PASS: 0, FAIL: 0}
+    @config.fetch("tests", {}).each do |k,v|
+      obj[:tests][k] = :PASS
+      obj[:tests][:PASS] += 1
+    end
     obj
   end
 end
