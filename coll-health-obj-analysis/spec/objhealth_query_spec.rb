6bd57488c36beaaf98bfc695460cfc89d5599507
Terry Brady <terrence.brady@ucop.edu>
2023-12-13 16:06:02 -0800

    add query rspec

--- coll-health-obj-analysis/spec/objhealth_query_spec.rb
+++ coll-health-obj-analysis/spec/objhealth_query_spec.rb
@@ -30,6 +30,128 @@ RSpec.describe 'object health query tests' do
       end
     end
 
+    describe "Pagination params" do
+      before(:each) do
+        @arks = []
+        @files = []
+        @urls = []
+        @fits_call = 0
+        allow_any_instance_of(ConsoleOutput).to receive(:output).and_wrap_original do |method, arg|
+          @arks.append(arg[:ark])
+          arg[:files].each do |f|
+            @files.append(f[:path])
+          end
+        end
+        allow_any_instance_of(FilesOutput).to receive(:output).and_wrap_original do |method, arg|
+          @arks.append(arg[:ark])
+          arg[:files].each do |f|
+            @urls.append(f[:url])
+          end
+        end
+        allow_any_instance_of(FitsOutput).to receive(:format_fits_output).and_wrap_original do |method, arg|
+          @fits_call += 1
+        end
+      end
+        
+      it "Default pagination params" do
+        ohq = ObjectHealthQuery.new([])
+        ohq.run_query
+        expect(@arks.length).to eq(10)
+      end
+
+      it "Set pagination limit" do
+        ohq = ObjectHealthQuery.new(['--limit=3'])
+        ohq.run_query
+        expect(@arks.length).to eq(3)
+      end
+
+      it "Set pagination start" do
+        ohq = ObjectHealthQuery.new(['--limit=10'])
+        ohq.run_query
+        expect(@arks.length).to eq(10)
+        ark_save = @arks[2]
+        @arks = []
+        ohq = ObjectHealthQuery.new(['--limit=10', '--start=2'])
+        ohq.run_query
+        expect(@arks.length).to eq(10)
+        expect(@arks[0]).to eq(ark_save)
+      end
+
+      it "Query for non-existent collection" do
+        ohq = ObjectHealthQuery.new(['--mnemonic=na'])
+        ohq.run_query
+        expect(@arks.length).to eq(0)
+      end
+
+      it "Query for known collection with one object" do
+        ohq = ObjectHealthQuery.new(['--mnemonic=cdl_ipresbo'])
+        ohq.run_query
+        expect(@arks.length).to eq(1)
+      end
+
+      it "Query for known ark" do
+        ohq = ObjectHealthQuery.new(['--ark=ark:/13030/m55v4d75'])
+        ohq.run_query
+        expect(@arks.length).to eq(1)
+        expect(@files.length).to eq(0)
+      end
+
+      it "Query for known ark (with 436 files), return files" do
+        ohq = ObjectHealthQuery.new(['--ark=ark:/13030/m55v4d75', '--fmt=files'])
+        ohq.run_query
+        expect(@arks.length).to eq(1)
+        expect(@files.length).to eq(436)
+      end
+
+      it "Query for known ark (with 436 files), return 10 files" do
+        ohq = ObjectHealthQuery.new(['--ark=ark:/13030/m55v4d75', '--fmt=files', '--max_file_per_object=10'])
+        ohq.run_query
+        expect(@arks.length).to eq(1)
+        expect(@files.length).to eq(10)
+      end
+
+      it "Query for known ark (with 436 files), return 10 file urls" do
+        ohq = ObjectHealthQuery.new(['--ark=ark:/13030/m55v4d75', '--fmt=files', '--output=files', '--max_file_per_object=10'])
+        ohq.run_query
+        expect(@arks.length).to eq(1)
+        expect(@urls.length).to eq(10)
+        @urls.each do |f|
+          expect(f).to match(%r[^https])
+        end
+      end
+
+      it "Query for known ark (with 436 files), return pdf files" do
+        ohq = ObjectHealthQuery.new(['--ark=ark:/13030/m55v4d75', '--fmt=files', '--file_mime_regex="\.pdf"'])
+        ohq.run_query
+        expect(@arks.length).to eq(1)
+        expect(@files.length).to be < 436
+        @files.each do |f|
+          expect(f).to match(%r[\.pdf])
+        end
+      end
+
+      it "Query for known ark (with 436 files), return non-existent zzzpdf files" do
+        ohq = ObjectHealthQuery.new(['--ark=ark:/13030/m55v4d75', '--fmt=files', '--file_mime_regex=\.zzzpdf'])
+        ohq.run_query
+        expect(@arks.length).to eq(1)
+        expect(@files.length).to eq(0)
+      end
+
+      it "Query for known ark (with 436 files), return file matched by path" do
+        ohq = ObjectHealthQuery.new(['--ark=ark:/13030/m55v4d75', '--fmt=files', '--file_path_regex=PLANETS_BROCHURE.pdf'])
+        ohq.run_query
+        expect(@arks.length).to eq(1)
+        expect(@files.length).to eq(1)
+      end
+
+      it "Query for known ark (with 436 files), call FITS for 2 files" do
+        allow($stdout).to receive(:write)
+        ohq = ObjectHealthQuery.new(['--ark=ark:/13030/m55v4d75', '--output=fits', '--max_file_per_object=2'])
+        ohq.run_query
+        expect(@fits_call).to eq(2)
+      end
+
+    end
   end
   
 end
