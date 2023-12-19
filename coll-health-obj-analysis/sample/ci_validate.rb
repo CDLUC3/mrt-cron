113cac6e9b86afae97b428047d61ca386a29951c
Terry Brady <terrence.brady@ucop.edu>
2023-12-19 15:05:15 -0800

    refine validation logic

---
+++ coll-health-obj-analysis/sample/ci_validate.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'json'
+require 'json-schema'
+require 'yaml'
+
+@val_errors = {}
+
+def validate(schema, doc, label)
+  stat = JSON::Validator.fully_validate(schema, doc)
+  return if stat.empty?
+  @val_errors[label] = stat
+  puts "Validation errors for #{label}"
+  stat.each do |s|
+    puts "- #{s}"
+  end
+  puts
+end
+
+schema_for_schema = JSON::Validator.validator_for_name('draft6').metaschema
+
+obj_schema = JSON.parse(File.read('config/obj_schema.json'))
+validate(schema_for_schema, obj_schema, 'Validate Object Schema')
+
+yaml_schema = JSON.parse(YAML.safe_load(File.read('config/yaml_schema.yml'), aliases: true).to_json)
+validate(schema_for_schema, yaml_schema, 'Validate Yaml Schema')
+
+config = JSON.parse(YAML.safe_load(File.read('config/merritt_classifications.yml'), aliases: true).to_json)
+validate(yaml_schema, config, 'Validate merritt_classifications.yml')
+
+exit 1 unless @val_errors.empty?
--- coll-health-obj-analysis/sample/validate_object.rb
+++ coll-health-obj-analysis/sample/validate_object.rb
@@ -1,32 +1,33 @@
+# frozen_string_literal: true
+
 require 'json'
 require 'json-schema'
 require 'yaml'
 
-obj = JSON.parse(File.read("sample/objects_details.json"))
-schema = JSON.parse(File.read("config/obj_schema.json"))
+obj = JSON.parse(File.read('sample/objects_details.json'))
+schema = JSON.parse(File.read('config/obj_schema.json'))
 stat = JSON::Validator.fully_validate(schema, obj)
-puts "valid" if stat.empty?
+puts 'valid' if stat.empty?
 puts stat unless stat.empty?
 
-obj = JSON.parse(File.read("sample/objects_details.bad.json"))
+obj = JSON.parse(File.read('sample/objects_details.bad.json'))
 stat = JSON::Validator.fully_validate(schema, obj)
-puts "valid" if stat.empty?
+puts 'valid' if stat.empty?
 puts stat unless stat.empty?
 
 schema_for_schema = JSON::Validator.validator_for_name('draft6').metaschema
 stat = JSON::Validator.fully_validate(schema_for_schema, schema)
-puts "valid" if stat.empty?
+puts 'valid' if stat.empty?
 puts stat unless stat.empty?
 
 yaml_schema = JSON.parse(YAML.safe_load(File.read('config/yaml_schema.yml'), aliases: true).to_json)
 config = JSON.parse(YAML.safe_load(File.read('config/merritt_classifications.yml'), aliases: true).to_json)
-stat = JSON::Validator.fully_validate(schema_for_schema, schema)
-puts "valid" if stat.empty?
+stat = JSON::Validator.fully_validate(yaml_schema, config)
+puts 'valid' if stat.empty?
 puts stat unless stat.empty?
 
 schema_for_schema = JSON::Validator.validator_for_name('draft6').metaschema
 yaml_schema = JSON.parse(YAML.safe_load(File.read('config/yaml_schema.yml'), aliases: true).to_json)
 stat = JSON::Validator.fully_validate(schema_for_schema, yaml_schema)
-puts "valid" if stat.empty?
+puts 'valid' if stat.empty?
 puts stat unless stat.empty?
-
