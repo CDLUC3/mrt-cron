14f3b2e05e8b072adc338be03e64d93c8d2b60f7
Terry Brady <terrence.brady@ucop.edu>
2024-01-17 10:28:08 -0800

    update mail send address

--- consistency-driver/driver.rb
+++ consistency-driver/driver.rb
@@ -113,7 +113,7 @@ class ConsistencyDriver
     d = `date "+%Y-%m-%d"`.chop
     msg = "#{@siteurl}?path=report&report=consistency-reports/#{d}"
     subj = "#{@status.upcase}: #{@mode} Consistency Report for #{d}"
-    `echo "#{msg}" | mail -s "#{subj}" #{@config.fetch('email', 'dpr2')}`
+    `echo "#{msg}" | mail -r uc3@cdlib.org -s "#{subj}" #{@config.fetch('email', 'dpr2')}`
   end
 end
 
