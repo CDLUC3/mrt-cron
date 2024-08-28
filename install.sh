c27d67b9e224f191ce60844a41de9c5098291dc0
Terry Brady <terrywbrady@gmail.com>
2024-08-28 10:34:00 -0700

    update test instructions

--- install.sh
+++ install.sh
@@ -18,11 +18,15 @@ echo '==========='
 bundle install
 
 # Stage
+# set SSM_ROOT
 # cd /dpr2/install/mrt-cron/consistency-driver && bundle exec ruby driver.rb
-# cd /dpr2/install/mrt-cron/coll-health-obj-analysis && bundle exec ruby object_health.rb
+# cd /dpr2/install/mrt-cron/coll-health-obj-analysis && bundle exec ruby object_health.rb -h
 
 # Prod
+# set SSM_ROOT
+# cd /dpr2/install/mrt-cron/consistency-driver && bundle exec ruby driver.rb
 # cd /dpr2/install/mrt-cron/viz && export COLLHDATA=/dpr2/apps/mrt-cron/coll_health/data && ./billing_viz.sh
 # cd /dpr2/install/mrt-cron/coll-health && export COLLHDATA=/dpr2/apps/mrt-cron/coll_health/data && ./mimefilelist.sh
+# cd /dpr2/install/mrt-cron/coll-health-obj-analysis && bundle exec ruby object_health.rb -h
 # cd /dpr2/install/mrt-cron/coll-health-obj-analysis && bundle exec ruby object_health.rb
 
