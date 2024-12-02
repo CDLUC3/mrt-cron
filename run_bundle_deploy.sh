6c8b16f60652ce890ae7cebb9529f01b43aa6926
Terry Brady <terrence.brady@ucop.edu>
2024-12-02 13:45:36 -0800

    deploy

---
+++ run_bundle_deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd coll-health
+echo
+pwd
+echo '==========='
+bundle install
+cd ../coll-health-obj-analysis
+echo
+pwd
+echo '==========='
+bundle install
+cd ../consistency-driver
+echo
+pwd
+echo '==========='
+bundle install
